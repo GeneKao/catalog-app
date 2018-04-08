502f761b90e968532cb059b253fc6e06cf227c81
Gene Kao <kao.gene@gmail.com>
2018-04-09 00:46:23 +0200

    Fix login bug.

--- app/application.py
+++ app/application.py
@@ -180,8 +180,11 @@ def getUserID(email):
         int: User ID.
 
     """
-    user = session.query(User).filter_by(email=email).one()
-    return user.id
+    try:
+        user = session.query(User).filter_by(email=email).one()
+        return user.id
+    except:
+        return None
 
 
 # DISCONNECT - Revoke a current user's token and reset their login_session
